29b44878b23a985460df4b9cd6f91202a9feeca2
Phil Quinn <phil.quinn@watchfinder.co.uk>
2021-12-05 17:03:12 +0000

    Refactored to walk between points and use tuples to track locations

--- day_05.py
+++ day_05.py
@@ -1,24 +1,5 @@
 import utils
 
-
-class Square:
-    def __init__(self, number):
-        self.number = int(number)
-        self.is_called = False
-
-class Board:
-    def __init__(self, numbers, x_size=5, y_size=5):
-        self.squares = []
-        self.x_size = x_size
-
-    def __repr__(self):
-        return str(self.squares)
-
-
-def make_boards(data):
-    all_numbers = data
-
-
 def get_vents(data):
     vents = []
     for line in data:
@@ -34,88 +15,57 @@ def get_vents(data):
 
     return vents
 
-def get_map(vents):
-    max_x = max([v['x1'] for v in vents] + [v['x2'] for v in vents])
-    max_y = max([v['y1'] for v in vents] + [v['y2'] for v in vents])
-    map = [[0 for i in range(max_x + 1)] for j in range(max_y + 1)]
 
+def get_vent_points(vent, with_diagonal=False):
+    vent_points = []
+
+    # Horizontal
+    if vent['y1'] == vent['y2']:
+        dx = int((vent['x2'] - vent['x1']) / abs(vent['x2'] - vent['x1']))
+        dy = 0
+
+    # Vertical
+    elif vent['x1'] == vent['x2']:
+        dy = int((vent['y2'] - vent['y1']) / abs(vent['y2'] - vent['y1']))
+        dx = 0
+
+    # Diagonal
+    elif with_diagonal and abs(vent['x1'] - vent['x2']) == abs(vent['x1'] - vent['x2']):
+        dx = int((vent['x2'] - vent['x1']) / abs(vent['x2'] - vent['x1']))
+        dy = int((vent['y2'] - vent['y1']) / abs(vent['y2'] - vent['y1']))
+
+    # Nothing to do
+    else:
+        return vent_points
+
+    # Have directions, now just walk to next point
+    ax = vent['x1']
+    ay = vent['y1']
+    vent_points.append((ax, ay))
+
+    while (ax != vent['x2'] or ay != vent['y2']):
+        ax += dx
+        ay += dy
+        vent_points.append((ax, ay))
+
+    return vent_points
+
+def get_map(vents, with_diagonal=False):
+    vent_locations = {}
     for vent in vents:
-        # Horizontal
-        if vent['x1'] == vent['x2']:
-            x = vent['x1']
-            y_min = min(vent['y1'], vent['y2'])
-            y_max = max(vent['y1'], vent['y2'])
-            for y in range (y_min, y_max + 1):
-                map[y][x] += 1
-        # Vertical
-        if vent['y1'] == vent['y2']:
-            y = vent['y1']
-            x_min = min(vent['x1'], vent['x2'])
-            x_max = max(vent['x1'], vent['x2'])
-            for x in range(x_min, x_max + 1):
-                map[y][x] += 1
-    return map
+        vent_points = get_vent_points(vent, with_diagonal=with_diagonal)
+        for vent_point in vent_points:
+            if vent_point not in vent_locations:
+                vent_locations[vent_point] = 0
+            vent_locations[vent_point] += 1
+    return vent_locations
 
-def get_map2(vents):
-    max_x = max([v['x1'] for v in vents] + [v['x2'] for v in vents])
-    max_y = max([v['y1'] for v in vents] + [v['y2'] for v in vents])
-    map = [[0 for i in range(max_x + 1)] for j in range(max_y + 1)]
-
-    for vent in vents:
-        found = False
-        # Horizontal
-        if vent['x1'] == vent['x2']:
-            x = vent['x1']
-            y_min = min(vent['y1'], vent['y2'])
-            y_max = max(vent['y1'], vent['y2'])
-            for y in range (y_min, y_max + 1):
-                map[y][x] += 1
-                found = True
-        # Vertical
-        if vent['y1'] == vent['y2']:
-            y = vent['y1']
-            x_min = min(vent['x1'], vent['x2'])
-            x_max = max(vent['x1'], vent['x2'])
-            for x in range(x_min, x_max + 1):
-                map[y][x] += 1
-                found = True
-
-        # Diagonal
-        x_d = vent['x1'] - vent['x2']
-        y_d = vent['y1'] - vent['y2']
-        if not found and abs(x_d) == abs(y_d):
-            # Take first point and walk in direction of second
-            ax = vent['x1']
-            ay = vent['y1']
-            map[ay][ax] += 1
-            while ax != vent['x2'] or ay != vent['y2']:
-                if ax < vent['x2']:
-                    ax += 1
-                elif ax > vent['x2']:
-                    ax -= 1
-                if ay < vent['y2']:
-                    ay += 1
-                elif ay > vent['y2']:
-                    ay -= 1
-                map[ay][ax] += 1
-
-
-
-
-    return map
-
-def display_map(map):
-    for x in map:
-        for y in x:
-            print(f"{y:02d} ", end='')
-        print("\n")
 
 def count_overlaps(map):
     overlaps = 0
-    for x in map:
-        for y in x:
-            if y > 1:
-                overlaps += 1
+    for p in map:
+        if map[p] > 1:
+            overlaps += 1
 
     return overlaps
 
@@ -129,11 +79,8 @@ def main():
     print('** Test 01 **')
     vents = get_vents(data_test)
     map = get_map(vents)
-    # display_map(map)
     overlaps = count_overlaps(map)
     print(f"{overlaps=}")
-
-
 
     with open (f'input/day_{day:02d}.txt') as fh:
         data = utils.lines(fh.read())
@@ -148,13 +95,13 @@ def main():
 
     print('** Test 02 **')
     vents = get_vents(data_test)
-    map = get_map2(vents)
+    map = get_map(vents, with_diagonal=True)
     overlaps = count_overlaps(map)
     print(f"{overlaps=}")
 
     print('** Part 02 **')
     vents = get_vents(data)
-    map2 = get_map2(vents)
+    map2 = get_map(vents, with_diagonal=True)
     overlaps = count_overlaps(map2)
     print(f"{overlaps=}")
 
