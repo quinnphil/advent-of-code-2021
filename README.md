4524ece13e52862a1f278fb016946102809d1602
Phil Quinn <phil.quinn@watchfinder.co.uk>
2021-12-15 14:17:46 +0000

    Day 15

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Python 3.9
 
 | Stars | Day | Title                                                           | P1 Time  | P1 Rank | P2 Time  | P2 Rank | Notes                |
 |:------|:----|:----------------------------------------------------------------|:---------|:--------|:---------|:--------|:---------------------|
+| ⭐⭐    | 15  | [Chiton](https://adventofcode.com/2021/day/15)                  | 01:22:01 | 4881    | 01:39:19 | 3326    | Heap Queue           |
 | ⭐⭐    | 14  | [Extended Polymerization](https://adventofcode.com/2021/day/14) | 00:29:15 | 5093    | 02:23:52 | 6587    | Polymers NBCH        |
 | ⭐⭐    | 13  | [Transparent Origami](https://adventofcode.com/2021/day/13)     | 00:54:25 | 5935    | 00:55:38 | 4770    | Folding dots         |
 | ⭐⭐    | 12  | [Passage Pathing](https://adventofcode.com/2021/day/12)         | 02:29:37 | 8128    | 02:53:28 | 7422    | Big cave little cave |
@@ -41,6 +42,9 @@ Python 3.9
 
 [[GitHub] GeertLitjens - AoC2021](https://github.com/GeertLitjens/advent-of-code-2021)
 - Nice explanations
+
+[[GitHub] IanFindlay - Aoc](https://github.com/IanFindlay/advent-of-code)
+- Python
 
 ### Credits
 
---
+++ day_15.py
@@ -0,0 +1,99 @@
+import heapq
+
+def get_adjacent_points(data, x, y):
+    for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        x1, y1 = x + dy, y + dx
+        # If within bounds yield coordinate
+        if 0 <= x1 < len(data) and 0 <= y1 < len(data[0]):
+            yield x1, y1
+
+
+def find_lowest_risk(grid, width, height):
+
+    # It costs nothing to be at 0, 0
+    pqueue = [(0, (0, 0))]
+
+    # Visited 0, 0
+    visited = {(0, 0)}
+
+    while pqueue:
+        # Get smallest item on heap (always the root)
+        distance, (x, y) = heapq.heappop(pqueue)
+
+        # Check is the last position if yes, return the distance
+        if (x == height - 1) and (y == width - 1):
+            return distance
+
+        # Otherwise check adjacent points
+        for x1, y1 in get_adjacent_points(grid, x, y):
+            # If not visited
+            if (x1, y1) not in visited:
+                # Cost equals current distance + new distance
+                heapq.heappush(pqueue, (distance + grid[x1][y1], (x1, y1)))
+                visited.add((x1, y1))
+
+def main():
+    day = 15
+
+    with open(f'input/day_{day:02d}_test.txt') as fh:
+        data_test = [[int(x) for x in line] for line in fh.read().splitlines()]
+
+    with open(f'input/day_{day:02d}.txt') as fh:
+        data = [[int(x) for x in line] for line in fh.read().splitlines()]
+
+
+
+    runs = [
+        {
+            "name": "** Test 01 **",
+            "data": data_test,
+            "assert_value": 10
+
+        },
+        {
+            "name": "** Part 01 **",
+            "data": data,
+
+        },
+
+
+    ]
+
+
+
+    for run in runs:
+
+        print(run['name'])
+        width = len(run['data'][0])
+        height = len(run['data'])
+
+        print(f'{width=}')
+        print(f'{height=}')
+
+        risk = find_lowest_risk(run['data'], width, height)
+
+        print(f'{risk=}')
+
+        new_grid = [line * 5 for line in run['data'] * 5]
+        for x in range(height * 5):
+            for y in range(width * 5):
+
+                old_v = new_grid[x][y]
+                new_v = old_v + (x // height) + (y // width) - 1 # Compensate to keep first tile the same
+                new_v = new_v % 9 + 1   # Increment and wrap
+                new_grid[x][y] = new_v
+
+
+        print(find_lowest_risk(new_grid, width * 5, height * 5))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
